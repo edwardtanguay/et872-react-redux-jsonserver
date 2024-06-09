cc9fb430c88db35e0cca097e98db3f01ad064bb3
Edward Tanguay (windows) <edward@tanguay.info>
2024-06-10 00:38:17 +0200

    rank buttons work

--- src/common/pages/PageWelcome.tsx
+++ src/common/pages/PageWelcome.tsx
@@ -17,12 +17,28 @@ export const PageWelcome = () => {
 				Welcome to the Redux Book Shop, current mode is{" "}
 				{appData.siteEnvironment}.
 			</p>
-				<p className="text-xl mb-2">Please rank the site from 1 to 10:</p>
-			<div className="flex gap-1">
-				<button className="symbols w-8 text-2xl" onClick={() => dispatch(subtract())}>-</button>
-				<button className="symbols w-8 text-2xl" onClick={() => dispatch(add())}>+</button>
-				<p className="ml-2 text-2xl">{count}</p>
-			</div>
+			<section className="border w-fit border-slate-500 p-3 rounded flex flex-col justify-center align-middle">
+				<p className="text-xl mb-2">
+					Please rank the site from 1 to 10:
+				</p>
+				<div className="flex gap-1 w-full justify-center">
+					<section className="flex gap-1  w-[7.5rem]">
+						<button
+							className="symbols w-8 text-2xl"
+							onClick={() => dispatch(subtract())}
+						>
+							-
+						</button>
+						<button
+							className="symbols w-8 text-2xl"
+							onClick={() => dispatch(add())}
+						>
+							+
+						</button>
+						<p className="ml-2 text-2xl">{count}</p>
+					</section>
+				</div>
+			</section>
 		</>
 	);
 };
