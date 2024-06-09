a3b0349e119d302142180bdac02c2344f80103f3
Edward Tanguay (windows) <edward@tanguay.info>
2024-06-09 23:05:03 +0200

    site working again

--- src/shared/interfaces.ts
+++ src/shared/interfaces.ts
@@ -1,22 +1,9 @@
-export interface IFlashcard {
-	id: number;
-	category: string;
-	front: string
-	back: string;
-}
-
 export type ISiteEnvironment = "normal" | "debug";
 
 export interface IAppData {
-	flashcards: IFlashcard[],
 	siteEnvironment: ISiteEnvironment
 }
 
 export const initialAppData:IAppData = {
-	flashcards: [],
 	siteEnvironment: 'normal'
-}
-
-export interface IDb {
-	flashcards: IFlashcard[]
 }
