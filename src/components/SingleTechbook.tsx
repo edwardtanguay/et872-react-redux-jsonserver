baec7c8de49ffc71f68b17888d3638a0d9c84be8
Edward Tanguay (windows) <edward@tanguay.info>
2024-06-09 23:39:56 +0200

    flexed books

--- src/components/SingleTechbook.tsx
+++ src/components/SingleTechbook.tsx
@@ -6,7 +6,7 @@ interface IProps {
 
 export const SingleTechBook = ({ techBook }: IProps) => {
 	return (
-		<div className="bg-slate-500 w-[40rem] mb-2 p-5 rounded flex gap-3">
+		<div className="bg-slate-500 w-[30rem] mb-2 p-5 rounded flex gap-3">
 			<img src={`images/techBooks/${techBook.idCode}.jpg`} className="rounded w-[9rem] h-fit" />
 			<section>
 				<div key={techBook.id} className="text-[1.2rem] font-semibold">
--- src/pages/PageTechBooks.tsx
+++ src/pages/PageTechBooks.tsx
@@ -6,13 +6,15 @@ export const PageTechBooks = () => {
 	const { techBooks } = useContext(AppContext);
 	return (
 		<>
-			<p className="mb-3 text-xl">There are {techBooks.length} tech books.</p>
+			<p className="mb-3 text-xl">
+				There are {techBooks.length} tech books.
+			</p>
 
-			{techBooks.map(techBook => {
-				return (
-					<SingleTechBook techBook={techBook} />
-				)
-			})}
+			<section className="flex flex-wrap gap-3">
+				{techBooks.map((techBook) => {
+					return <SingleTechBook techBook={techBook} />;
+				})}
+			</section>
 		</>
 	);
 };
