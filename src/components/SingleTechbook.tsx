60521bbf5ee65bcf4d44dd72cfbf41be0e174f21
Edward Tanguay (windows) <edward@tanguay.info>
2024-06-09 23:35:20 +0200

    books in, one showing

--- src/components/SingleTechbook.tsx
+++ src/components/SingleTechbook.tsx
@@ -7,6 +7,7 @@ interface IProps {
 export const SingleTechBook = ({ techBook }: IProps) => {
 	return (
 		<div className="bg-slate-500 w-[40rem] mb-2 p-2 rounded">
+			<img src="images/techBooks/viAndVim.jpg"/>
 			<div key={techBook.id} className="text-[1.2rem] font-semibold">{techBook.title}</div>
 			<div className="italic">{techBook.description}</div>
 		</div>
