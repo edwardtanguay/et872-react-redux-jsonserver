46cf37f067923d4d21e5ef3ac28fd38deadeb07a
Edward Tanguay (windows) <edward@tanguay.info>
2024-06-09 23:33:57 +0200

    title desc in

--- src/components/SingleTechbook.tsx
+++ src/components/SingleTechbook.tsx
@@ -6,8 +6,9 @@ interface IProps {
 
 export const SingleTechBook = ({ techBook }: IProps) => {
 	return (
-		<>
-			<div key={techBook.id}>{techBook.title}</div>
-		</>
+		<div className="bg-slate-500 w-[40rem] mb-2 p-2 rounded">
+			<div key={techBook.id} className="text-[1.2rem] font-semibold">{techBook.title}</div>
+			<div className="italic">{techBook.description}</div>
+		</div>
 	);
 };
