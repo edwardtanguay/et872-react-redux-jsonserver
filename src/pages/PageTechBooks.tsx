579aebcbddebde770c1302ce8bf98e84165881e4
Edward Tanguay (windows) <edward@tanguay.info>
2024-06-09 23:41:43 +0200

    books showing with images with appContext

--- src/pages/PageTechBooks.tsx
+++ src/pages/PageTechBooks.tsx
@@ -6,15 +6,21 @@ export const PageTechBooks = () => {
 	const { techBooks } = useContext(AppContext);
 	return (
 		<>
-			<p className="mb-3 text-xl">
-				There are {techBooks.length} tech books.
-			</p>
+			{techBooks.length > 0 ? (
+				<>
+					<p className="mb-3 text-xl">
+						There are {techBooks.length} tech books.
+					</p>
 
-			<section className="flex flex-wrap gap-3">
-				{techBooks.map((techBook) => {
-					return <SingleTechBook techBook={techBook} />;
-				})}
-			</section>
+					<section className="flex flex-wrap gap-3">
+						{techBooks.map((techBook) => {
+							return <SingleTechBook techBook={techBook} />;
+						})}
+					</section>
+				</>
+			) : (
+				<div>loading...</div>
+			)}
 		</>
 	);
 };
