4bb2f21f12859f77166003c7514381b8187643f8
Edward Tanguay (windows) <edward@tanguay.info>
2024-06-09 23:36:50 +0200

    dynamic images in

--- src/components/SingleTechbook.tsx
+++ src/components/SingleTechbook.tsx
@@ -6,10 +6,14 @@ interface IProps {
 
 export const SingleTechBook = ({ techBook }: IProps) => {
 	return (
-		<div className="bg-slate-500 w-[40rem] mb-2 p-2 rounded">
-			<img src="images/techBooks/viAndVim.jpg"/>
-			<div key={techBook.id} className="text-[1.2rem] font-semibold">{techBook.title}</div>
-			<div className="italic">{techBook.description}</div>
+		<div className="bg-slate-500 w-[40rem] mb-2 p-5 rounded flex gap-3">
+			<img src={`images/techBooks/${techBook.idCode}.jpg`} className="rounded" />
+			<section>
+				<div key={techBook.id} className="text-[1.2rem] font-semibold">
+					{techBook.title}
+				</div>
+				<div className="italic">{techBook.description}</div>
+			</section>
 		</div>
 	);
 };
