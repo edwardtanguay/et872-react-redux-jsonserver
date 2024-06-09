91710c4346cf0060f48f29b9f8461147b77e9fb7
Edward Tanguay (windows) <edward@tanguay.info>
2024-06-10 00:41:20 +0200

    changing ranking works with limits

--- src/features/cart/cartSlice.ts
+++ src/features/cart/cartSlice.ts
@@ -1,20 +1,22 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-	rating: 5,
-};
+	rating: 5
+}
 
 export const cartSlice = createSlice({
-	name: "cart",
+	name: 'cart',
 	initialState,
 	reducers: {
 		subtract: (state) => {
 			state.rating--;
+			state.rating = state.rating < 0 ? 0 : state.rating;
 		},
 		add: (state) => {
 			state.rating++;
-		},
-	},
+			state.rating = state.rating > 10 ? 10 : state.rating;
+		}
+	}
 });
 
 export const { subtract, add } = cartSlice.actions;
