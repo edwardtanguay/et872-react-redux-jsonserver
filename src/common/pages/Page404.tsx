ad2da4999b6017bc3f65d7d9a08c331ac22c9201
Edward Tanguay (windows) <edward@tanguay.info>
2024-06-10 09:31:34 +0200

    Edward: made minor changes to notes and 404

--- src/common/pages/Page404.tsx
+++ src/common/pages/Page404.tsx
@@ -4,7 +4,7 @@ export const Page404 = () => {
 	return (
 		<div className="px-10 -my-8">
 		<p className="text-[10rem] font-mono text-slate-500">404</p>
-		<p className="-my-8 text-2xl">Please <NavLink to="/" className="underline">return to the site</NavLink>.</p>
+		<p className="-my-8 text-2xl">Please <NavLink to="/" className="underline">return to site</NavLink>.</p>
 		</div>
 	)
 }
