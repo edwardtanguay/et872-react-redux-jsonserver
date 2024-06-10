dd8f3bea262880a1a1812273f8df3ec2d501e9d4
Edward Tanguay (windows) <edward@tanguay.info>
2024-06-10 08:50:27 +0200

    showing books on cart

--- src/common/pages/PageShoppingCart.tsx
+++ src/common/pages/PageShoppingCart.tsx
@@ -1,14 +1,21 @@
 import { useSelector } from "react-redux";
 import { RootState } from "../../app/store";
+import { SingleTechBook } from "../components/SingleTechbook";
 
 export const PageShoppingCart = () => {
-	// const dispatch = useDispatch();
 	const cartSelector = (state: RootState) => state.cart;
 	const { cartItems } = useSelector(cartSelector);
 
 	return (
 		<>
 			<p>There are {cartItems.length} in your cart.</p>
+			<section className="flex flex-wrap gap-3">
+				{cartItems.map((cartItem) => {
+					return (
+						<SingleTechBook techBook={cartItem.techBook} key={cartItem.techBook.id} />
+					);
+				})}
+			</section>
 		</>
-	)
+	);
 };
