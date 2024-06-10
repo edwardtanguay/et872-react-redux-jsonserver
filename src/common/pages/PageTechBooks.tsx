4d0108cb424f27fd5f7f8cc7b3123b51d9af079e
Edward Tanguay (windows) <edward@tanguay.info>
2024-06-10 08:43:41 +0200

    showing both counters

--- src/common/pages/PageTechBooks.tsx
+++ src/common/pages/PageTechBooks.tsx
@@ -1,6 +1,7 @@
 import { useSelector } from "react-redux";
 import { RootState } from "../../app/store";
 import { SingleTechBook } from "../components/SingleTechbook";
+import { NavLink } from "react-router-dom";
 
 export const PageTechBooks = () => {
 	const selectCart = (state: RootState) => state.cart;
@@ -24,7 +25,18 @@ export const PageTechBooks = () => {
 							);
 						})}
 					</section>
-					<p className={cartItems.length > 0 ? 'text-yellow-200 font-semibold' : 'text-gray-400 italic'}>The are {cartItems.length} items the cart.</p>
+					<p
+						className={
+							cartItems.length > 0
+								? "text-yellow-300 font-semibold bg-gray-700 w-fit px-2 py-1 rounded"
+								: "text-gray-400 italic"
+						}
+					>
+						There are {cartItems.length} items in your {' '}
+						<NavLink to="/shoppingcart" className="underline">
+							cart
+						</NavLink> at the moment.
+					</p>
 				</>
 			) : (
 				<div>Loading...</div>
